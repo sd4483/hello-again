c1cdb878e6784b9a3fd8283e7eb54a90e147e904
sdcodes <paturi.subs@gmail.com>
2018-11-07 19:19:17 +0800

    #day30
    day 30 of #100daysofcode

--- 30th/02.py
+++ 30th/02.py
@@ -14,7 +14,7 @@ def insert(item, quantity, price):
     conn.commit()
     conn.close()
 
-#insert('Beer', 12, 2.4)
+insert('Beer', 12, 2.4)
 
 def view_db():
     conn = sqlite3.connect("lite.db")
@@ -24,20 +24,4 @@ def view_db():
     conn.close()
     return rows
 
-def delete_db(item):
-    conn = sqlite3.connect("lite.db")
-    cur = conn.cursor()
-    cur.execute("DELETE FROM store WHERE item=?", (item,))
-    conn.commit()
-    conn.close()
-
-def update_db(quantity, price, item):
-    conn = sqlite3.connect("lite.db")
-    cur = conn.cursor()
-    cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (quantity,price,item))
-    conn.commit()
-    conn.close()
-
-update_db(11,1.0, 'Water Bottle')
-#delete_db('Beer')
 print(view_db())
---
+++ 30th/30.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+def create_table():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    conn.commit()
+    conn.close()
+
+def insert(item, quantity, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES(?,?,?)", (item,quantity,price))
+    conn.commit()
+    conn.close()
+
+insert('Wine',5,24.0)
+
+create_table()
+
